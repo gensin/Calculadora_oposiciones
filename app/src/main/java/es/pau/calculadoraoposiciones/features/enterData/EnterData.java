55e53d7007935220a3dc5d7f412a42ba4fbadd54
Gensin <ghostdesert@gmail.com>
2016-06-22 00:53:07 +0200

    Add butterknife

--- app/src/main/java/es/pau/calculadoraoposiciones/features/enterData/EnterData.java
+++ app/src/main/java/es/pau/calculadoraoposiciones/features/enterData/EnterData.java
@@ -1,7 +1,14 @@
 package es.pau.calculadoraoposiciones.features.enterData;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import org.webpartners.meigic.views.MeigicActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import es.pau.calculadoraoposiciones.R;
 
 /**
@@ -9,11 +16,22 @@ import es.pau.calculadoraoposiciones.R;
  */
 public class EnterData extends MeigicActivity<EnterDataPresenter, EnterDataView> implements EnterDataView {
 
-    @Override protected EnterDataPresenter initPresenter() {
+    @BindView(R.id.total_topics) EditText totalTopics;
+    @BindView(R.id.taken_topics) EditText takenTopics;
+    @BindView(R.id.studied_topics) EditText studiedTopics;
+    @BindView(R.id.calculate) Button calculate;
+
+    @Override
+    protected EnterDataPresenter initPresenter() {
         return new EnterDataPresenter(this);
     }
 
-    @Override protected void initView() {
+    @Override
+    protected void initView() {
         setContentView(R.layout.activity_data);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.calculate) public void onCalculateButtonClick() {
     }
 }
